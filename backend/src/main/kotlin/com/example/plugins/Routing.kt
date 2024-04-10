e578c5ca0854482615a9ff7f16e6fe71d74c7f50
vityaman <vityaman.dev@yandex.ru>
2024-04-10 11:33:47 +0300

    [backend] reduced

--- backend/src/main/kotlin/com/example/plugins/Routing.kt
+++ backend/src/main/kotlin/com/example/plugins/Routing.kt
@@ -23,14 +23,5 @@ fun Application.configureRouting() {
         get("/project-name") {
             call.respondText(projectName())
         }
-
-        get("/test1") {
-            val text = "<h1>Hello From Ktor</h1>"
-            val type = ContentType.parse("text/html")
-            call.respondText(text, type)
-        }
-        get("/error-test") {
-            throw IllegalStateException("Too Busy")
-        }
     }
 }
